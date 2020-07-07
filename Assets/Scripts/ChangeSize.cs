ac99b719bc865a99abecced11b8ae4551e739251
Geoffrey Hil <g.hill@sae.edu>
2020-07-07 17:32:30 +1000

    Added TextMesh output for the scale, position, and rotation

--- Assets/Scripts/ChangeSize.cs
+++ Assets/Scripts/ChangeSize.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class ChangeSize : MonoBehaviour
 {
     public float rotateAmountDegrees;
-    
+
+    //In each script you will need this
+    public TextMesh HUD;
+
+
     // Start is called before the first frame update
     void Start()
     {
+
+
     }
 
     // Update is called once per frame
@@ -23,12 +29,14 @@ public class ChangeSize : MonoBehaviour
 
         transform.localScale = new UnityEngine.Vector3(transform.localScale.x + 1, transform.localScale.y + 1, transform.localScale.z + 1);
 
+        HUD.text = transform.localScale.ToString(); //update the HUD text after making a change.
 
     }
 
     public void MakeSmall()
     {
         transform.localScale = new UnityEngine.Vector3(transform.localScale.x - 1, transform.localScale.y - 1, transform.localScale.z - 1);
+        HUD.text = transform.localScale.ToString(); //update the HUD text.
 
 
     }
@@ -37,22 +45,29 @@ public class ChangeSize : MonoBehaviour
     {
 
         transform.Rotate(0.0f, -rotateAmountDegrees, 0.0f, Space.Self);
+        HUD.text = transform.rotation.ToString();
+
     }
 
     public void RotateLeft()
     {
 
         transform.Rotate(0.0f, rotateAmountDegrees, 0.0f, Space.Self);
+        HUD.text = transform.rotation.ToString();
+
     }
 
     public void MoveLeft()
     {
         transform.Translate(UnityEngine.Vector3.left);
+        HUD.text = transform.position.ToString();
+
     }
 
     public void MoveRight()
     {
         transform.Translate(UnityEngine.Vector3.right);
+        HUD.text = transform.position.ToString();
 
     }
 }
