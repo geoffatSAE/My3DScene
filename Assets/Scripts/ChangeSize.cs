1f66fafe20e3a3bc3724d3f79b99a3942b936f6b
Geoffrey Hil <g.hill@sae.edu>
2020-07-07 12:00:38 +1000

    Initial commit.

---
+++ Assets/Scripts/ChangeSize.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSize : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MakeBig()
+    {
+
+        transform.localScale = new Vector3(transform.localScale.x + 1, transform.localScale.y + 1, transform.localScale.z + 1);
+
+
+    }
+
+    public void MakeSmall()
+    {
+        transform.localScale = new Vector3(transform.localScale.x - 1, transform.localScale.y - 1, transform.localScale.z - 1);
+
+
+    }
+}
