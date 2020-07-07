d78499ed1514dda5c8faabe3b964fa6e591a164e
Geoffrey Hil <g.hill@sae.edu>
2020-07-07 15:58:08 +1000

    Added rotation and translaton and a giraffe.

--- Assets/Scripts/ChangeSize.cs
+++ Assets/Scripts/ChangeSize.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class ChangeSize : MonoBehaviour
 {
-    
+    public float rotateAmountDegrees;
     
     // Start is called before the first frame update
     void Start()
@@ -20,15 +21,38 @@ public class ChangeSize : MonoBehaviour
     public void MakeBig()
     {
 
-        transform.localScale = new Vector3(transform.localScale.x + 1, transform.localScale.y + 1, transform.localScale.z + 1);
+        transform.localScale = new UnityEngine.Vector3(transform.localScale.x + 1, transform.localScale.y + 1, transform.localScale.z + 1);
 
 
     }
 
     public void MakeSmall()
     {
-        transform.localScale = new Vector3(transform.localScale.x - 1, transform.localScale.y - 1, transform.localScale.z - 1);
+        transform.localScale = new UnityEngine.Vector3(transform.localScale.x - 1, transform.localScale.y - 1, transform.localScale.z - 1);
 
+
+    }
+
+    public void RotateRight()
+    {
+
+        transform.Rotate(0.0f, -rotateAmountDegrees, 0.0f, Space.Self);
+    }
+
+    public void RotateLeft()
+    {
+
+        transform.Rotate(0.0f, rotateAmountDegrees, 0.0f, Space.Self);
+    }
+
+    public void MoveLeft()
+    {
+        transform.Translate(UnityEngine.Vector3.left);
+    }
+
+    public void MoveRight()
+    {
+        transform.Translate(UnityEngine.Vector3.right);
 
     }
 }
