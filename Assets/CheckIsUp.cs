5a5ca60ae89798da430bf0288b436368adb6209a
geoffatSAE <g.hill@sae.edu>
2020-09-18 14:04:49 +1000

    Up scene and script

---
+++ Assets/CheckIsUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckIsUp : MonoBehaviour
+{
+    public float tolerance = 0.9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsUpsideDown()){
+            
+            Debug.Log("Is upside down");
+        } else {
+            Debug.Log("Is Right side up");
+        }
+    }
+
+    public bool IsUpsideDown()
+    {
+     return (Vector3.Dot(transform.up, Vector3.up) < -tolerance);
+    }
+}
